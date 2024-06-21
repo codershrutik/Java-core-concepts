284f9d5c8b26e2bf213e22bb291e6198ce8ff62f
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-21 15:56:45 +0530

    finally added

--- src/Main.java
+++ src/Main.java
@@ -49,24 +49,39 @@ public class Main{
 	// Child child = new Child();
 	// child.India();
 	// child.USA();
-	Main main = new Main();
-	main.case1(10, 5);
-	main.case2(10, 0);
+	// Main main = new Main();
+	// main.case1(10, 5);
+	// main.case2(10, 0);
+
+	int x;
+	int y=0;
+
+	try {
+		System.out.println("before exception");
+		x=(y=10*10)/0;
+		System.out.println("after exception");
+	} catch (ArithmeticException e) {
+		System.out.println("arithmatic exception");
+	} catch (Exception e){
+		System.out.println("exception");
+	} finally{
+		System.out.println(y);
+	}
     }
 
-	public void case1(int x, int y){
-		if(y!=0){
-			System.out.println("x/y="+(x/y));
-		} else{
-			System.out.println("The value of y is 0, a possible exception");
-		}
-	}
+	// public void case1(int x, int y){
+	// 	if(y!=0){
+	// 		System.out.println("x/y="+(x/y));
+	// 	} else{
+	// 		System.out.println("The value of y is 0, a possible exception");
+	// 	}
+	// }
 
-	public void case2(int x, int y){
-		try {
-			System.out.println("x/y="+(x/y));
-		} catch (Exception e) {
-			System.out.println("The value of y is 0, a possible exception"+e);
-		}
-	}
+	// public void case2(int x, int y){
+	// 	try {
+	// 		System.out.println("x/y="+(x/y));
+	// 	} catch (Exception e) {
+	// 		System.out.println("The value of y is 0, a possible exception"+e);
+	// 	}
+	// }
 }
