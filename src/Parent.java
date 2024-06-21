00961e71d4f47c5383bbc848892622ea4889c9c9
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-21 15:24:42 +0530

    Parent class added

---
+++ src/Parent.java
@@ -0,0 +1,9 @@
+public class Parent {
+    public void India(){
+        System.out.println("India is a country");
+    }
+
+    public void USA(){
+        System.out.println("USA is a country!");
+    }
+}
