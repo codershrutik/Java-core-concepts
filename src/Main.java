d8b9dfb1fb6024d2e21244e41581eebe69bbc2f8
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-21 16:47:08 +0530

    Enum catching values

--- src/Main.java
+++ src/Main.java
@@ -79,14 +79,16 @@ public class Main{
 	// 	e.printStackTrace();
 	// }
 	Learning learning = Learning.GENERICS;
-	switch(learning){
-		case COREJAVA -> System.out.println("Core java");
-		case COLLECTIONS -> System.out.println("Collections");
-		case GENERICS -> System.out.println("Generics");
-		case JSP -> System.out.println("JSP");
-		case MULTITHREADING -> System.out.println("Multithreading");
-	}
-	System.out.println(learning.COREJAVA);
+	// switch(learning){
+	// 	case COREJAVA -> System.out.println("Core java");
+	// 	case COLLECTIONS -> System.out.println("Collections");
+	// 	case GENERICS -> System.out.println("Generics");
+	// 	case JSP -> System.out.println("JSP");
+	// 	case MULTITHREADING -> System.out.println("Multithreading");
+	// }
+	// System.out.println(learning.COREJAVA);
+	System.out.println(learning.COLLECTIONS);
+	System.out.println(learning.COREJAVA.getI());
     }
 	// public void doSomething() throws ZeroException{
 	// 	// FileReader in = new FileReader("text.txt");
