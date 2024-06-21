61a444a559ec3ea95c85ffbdf9f0c2b44f3739b1
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-21 15:38:54 +0530

    String added in java

---
+++ src/Stringg.java
@@ -0,0 +1,8 @@
+public class Stringg {
+    public static void main(String[] args) {
+        String a = "Java";
+        String b = "Language";
+        String c = a+b;
+        System.out.println(c);
+    }
+}
